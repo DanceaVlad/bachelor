f6b9f27da700e6a237e69c91ea3499b40d6f06cb
dancea <e12123455@student.tuwien.ac.at>
2025-01-08 08:56:28 +0100

    Microservice: Change file output names

--- microservice/src/main/java/com/dancea/microservice/Utils.java
+++ microservice/src/main/java/com/dancea/microservice/Utils.java
@@ -23,11 +23,11 @@ public class Utils {
     public static final String COLLECTION_ID = "modis-13A1-061";
     public static final String PLANETARY_COMPUTER_SIGN_URL = "https://planetarycomputer.microsoft.com/api/sas/v1/sign";
     public static final String PLANETARY_COMPUTER_SEARCH_URL = "https://planetarycomputer.microsoft.com/api/stac/v1/search";
-    public static final String TILE_OUTPUT_DIR = "microservice/src/main/resources/tiles/";
-    public static final String TIFF_OUTPUT_DIR = "microservice/src/main/resources/tiffs/";
-    public static final String MERGE_OUTPUT_DIR = "microservice/src/main/resources/merged.tif";
-    public static final String REPROJECTED_OUTPUT_DIR = "microservice/src/main/resources/reprojected.tif";
-    public static final String BYTE_OUTPUT_DIR = "microservice/src/main/resources/byte.vrt";
+    public static final String TIFF_OUTPUT_DIR = "microservice/src/main/resources/01_tiffs/";
+    public static final String MERGE_OUTPUT_DIR = "microservice/src/main/resources/02_merged.tif";
+    public static final String REPROJECTED_OUTPUT_DIR = "microservice/src/main/resources/03_reprojected.tif";
+    public static final String BYTE_OUTPUT_DIR = "microservice/src/main/resources/04_byte.vrt";
+    public static final String TILE_OUTPUT_DIR = "microservice/src/main/resources/05_tiles/";
     public static final String TOKEN_KEY = "token";
 
     private static final Logger logger = LoggerFactory.getLogger(Utils.class);
